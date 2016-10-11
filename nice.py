6fe27a58dfe03ccfbe8c2d8cb5b4428e7a70142c
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-11 12:58:52 -0700

    added response and age field

--- nice.py
+++ nice.py
@@ -31,8 +31,10 @@ def say_hello():
       </head>
       <body>
         <h1>Hi There!</h1>
+
         <form action="/greet">
           <label>What's your name? <input type="text" name="person"></label>
+          <label>Age? <input type="text" name="age"></label>
           <select name="compliment">
             <option value="awesome">awesome</option>
             <option value="terrific">terrific</option>
@@ -51,8 +53,13 @@ def say_hello():
           </select>
           <input type="submit">
         </form>
+
         <form action="/diss">
           <label>What's your name? <input type="text" name="person"></label>
+          <label>Adjective?
+            <input type="radio" name="adj" value="very">very
+            <input type="radio" name="adj" value="extremely">extremely
+            <input type="radio" name="adj" value="super">super
           <select name="diss">
             <option value="bad">bad</option>
             <option value="mean">mean</option>
@@ -71,6 +78,7 @@ def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
+    age_num = int(request.args.get("age"))
 
     compliment = request.args.get("compliment")
     return """
@@ -83,7 +91,7 @@ def greet_person():
         Hi %s I think you're %s!
       </body>
     </html>
-    """ % (player, compliment)
+    """ % (player, compliment * age_num)
 
 
 @app.route('/diss')
@@ -91,7 +99,7 @@ def diss_person():
     """Get user by name."""
 
     player = request.args.get("person")
-
+    adjective = request.args.get("adj")
     diss = request.args.get("diss")
     return """
     <!doctype html>
@@ -100,10 +108,33 @@ def diss_person():
         <title>A Diss</title>
       </head>
       <body>
-        Hi %s I think you're %s!
+        Hi %s I think you're %s %s!<br>
+        <form action="/respond">
+          <label>Why are you not %s? <input type="text" name="whynot"></label>
+          <input type="submit">
+        </form>
       </body>
     </html>
-    """ % (player, diss)
+    """ % (player, adjective, diss, diss)
+
+@app.route('/respond')
+def response():
+    """Justify yourself."""
+
+    response = request.args.get("whynot")
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Justification</title>
+      </head>
+      <body>
+        %s is a good reason.<br>
+        I apologize.
+      </body>
+    </html>
+    """ % response
+
 
 
 if __name__ == '__main__':
